23b76f5bfb8aa3654b55d8775f6c0c9c49ed22a5
Carl Victor Fontanos <carl.fontanos@gmai.com>
2018-10-25 15:44:17 +0800

    Integrated lazy loading with router via deep linker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,42 +3,16 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { WidgetsPage } from '../pages/widgets/widgets';
-import { EcommercePage } from '../pages/ecommerce/ecommerce';
-import { MailboxPage } from '../pages/mailbox/mailbox';
-import { ChatPage } from '../pages/chat/chat';
-import { ProfilePage } from '../pages/profile/profile';
-
 @Component({
 	templateUrl: 'app.html'
 })
 export class MyApp {
 	@ViewChild(Nav) nav: Nav;
 
-	rootPage: any = HomePage;
-
-	pages: Array<{
-		title: string, 
-		component: any,
-		icon_ios: string,
-		icon_md: string
-	}>;
+	rootPage: any = 'HomePage'; /* Lazy loading: load all pages as string */
 
 	constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
 		this.initializeApp();
-
-		// used for an example of ngFor and navigation
-		this.pages = [
-			{ title: 'Home', component: HomePage, icon_ios: 'stats-outline', icon_md: 'stats' },
-			{ title: 'List', component: ListPage, icon_ios: 'list-outline', icon_md: 'list' },
-			{ title: 'Widgets', component: WidgetsPage, icon_ios: 'apps-outline', icon_md: 'apps' },
-			{ title: 'Ecommerce', component: EcommercePage, icon_ios: 'cart-outline', icon_md: 'cart' },
-			{ title: 'Mailbox', component: MailboxPage, icon_ios: 'mail-outline', icon_md: 'mail' },
-			{ title: 'Chat', component: ChatPage, icon_ios: 'chatboxes-outline', icon_md: 'chatboxes' },
-			{ title: 'Profile', component: ProfilePage, icon_ios: 'person-outline', icon_md: 'person' }
-		];
 	}
 
 	initializeApp() {
@@ -53,6 +27,6 @@ export class MyApp {
 	openPage(page) {
 		// Reset the content nav to have just this page
 		// we wouldn't want the back button to show in this scenario
-		this.nav.setRoot(page.component);
+		this.nav.setRoot(page);
 	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,6 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { WidgetsPage } from '../pages/widgets/widgets';
-import { EcommercePage } from '../pages/ecommerce/ecommerce';
-import { MailboxPage } from '../pages/mailbox/mailbox';
-import { MailboxSinglePage } from '../pages/mailbox-single/mailbox-single';
-import { ChatPage } from '../pages/chat/chat';
-import { ProfilePage } from '../pages/profile/profile';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,14 +10,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
 	declarations: [
 		MyApp,
-		HomePage,
-		ListPage,
-		WidgetsPage,
-		EcommercePage,
-		MailboxPage,
-		MailboxSinglePage,
-		ChatPage,
-		ProfilePage
 	],
 	imports: [
 		BrowserModule,
@@ -36,14 +20,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 	bootstrap: [IonicApp],
 	entryComponents: [
 		MyApp,
-		HomePage,
-		ListPage,
-		WidgetsPage,
-		EcommercePage,
-		MailboxPage,
-		MailboxSinglePage,
-		ChatPage,
-		ProfilePage
 	],
 	providers: [
 		StatusBar,
