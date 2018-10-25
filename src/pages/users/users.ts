caf4c1d71cd24990e30c2cd9678ac65d0625feeb
Carlo Fontanos <carl.fontanos@gmail.com>
2018-10-25 23:41:27 +0800

    Added progress for infinite scroll

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,47 +1,56 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
-	selector: 'page-users',
-	templateUrl: 'users.html',
+    selector: 'page-users',
+    templateUrl: 'users.html',
 })
 export class UsersPage {
-	constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public loadingCtrl: LoadingController) {
-	}
-	
-	users: any;
-	
-	loadJson() {
-		const loader = this.loadingCtrl.create({
-            content: "Please wait...",
-        });
+    hasMoreItems = 1;
+    lastItem = 0;
+    limit = 20;
+    users = [];
 
-        loader.present();
-
+    constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    }
+    
+    loadJson() {
         this.http.get('https://randomuser.me/api/?results=30')
-			.map(res => res.json())
-			.subscribe(res => {
-				this.users = res.results;
-                loader.dismiss();
-			}, (err) => {
-				alert("failed loading json data");
-			}
-		);
-	}
-	
-	itemTapped(event, user) {
-		this.navCtrl.push('UsersSinglePage', {
-			id: user.login.uuid, 
-			user: user
-		});
-	}
+            .map(res => res.json())
+            .subscribe(res => {
+                for(let data of res.results) {
+                    this.users.push(data);
+                }
+            }, (err) => {
+                alert("failed loading json data");
+            }
+        );
+        return true;
+    }
 
-	ionViewDidLoad() {
-		this.loadJson();
-		console.log('ionViewDidLoad UsersPage');
-	}
+    doInfinite(): Promise<any> {
+        console.log('Begin async operation');
+
+        return new Promise((resolve) => {
+            if(this.loadJson()){
+                resolve();
+            }
+        })
+    }
+    
+    itemTapped(event, user) {
+        this.navCtrl.push('UsersSinglePage', {
+            id: user.login.uuid, 
+            user: user
+        });
+    }
+
+    ionViewDidLoad() {
+        this.loadJson();
+        console.log('ionViewDidLoad UsersPage');
+    }
 
 }
